ea87990a6978ca316ab31a7c67b5a085d1b07bf3
arnasarn <sirvydasarnas@gmail.com>
2026-09-14 11:51:44 +0300

    f

---
+++ LD1/LD1_ArnasSirvydas.m
@@ -0,0 +1,26 @@
+%
+% Paprastas skriptas
+%
+
+x = 1:32;
+y = x.^2;
+
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X-ai')
+ylabel('F_1  [-o-]  |  F_2 [-x-]')
+
+% help sin
+% doc plot
+% docsearch title
+
+N = 2;
+vekt = N+1:0.5:N+4
+
+A = [N N+1 N+2; N+3 N+4 N+5; N+6 N+7 N+8];
+A(3, 2)
+A(2:3, 1:2)
+A([1 3 7 9])
+A([1 3], [1 3])
+
+B=[A; vekt(1:3)]
